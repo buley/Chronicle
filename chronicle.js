dcea89233054618bb855d1072b55a1204f7f4988
= <=>
2012-04-06 23:21:33 -0400

    ...

--- chronicle.js
+++ chronicle.js
@@ -60,9 +60,9 @@ var Chronicle = ( function() {
 			count += 1;
 			results.push( response );
 			if( 2 === count ) {
+				db_ver = 2;
+				window.localStorage.setItem( db_ver_key, 2 );
 				if( 'function' === typeof on_success ) {
-					window.localStorage.setItem( db_ver_key, 2 );
-					db_ver = 2;
 					on_success( results );
 				}
 			}
